4107f95999b9bdf5a46bf630c0b2872605734d82
Edmundo0994 <esala.094@gmail.com>
2022-02-17 16:23:38 -0500

    :sparkles: [ADD] Alias with webpack and starting using hooks

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import ProductItem from '../components/ProductItem';
-import '../styles/ProductList.scss';
+import React, { useEffect, useState } from 'react';
+import ProductItem from '@components/ProductItem';
+import '@styles/ProductList.scss';
+import axios from 'axios';
+
+const API = "https://api.escuelajs.co/api/v1/products"
 
 const ProductList = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(async () => {
+    try {
+      const response = await axios(API)
+      setProducts(response.data)
+    } catch(err) {
+      console.log(err)
+    }
+  }, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+        {products.length > 0 && products.map(product => <ProductItem />)}
 			</div>
 		</section>
 	);
