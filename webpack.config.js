729617c363fd7065f874c909f5a7c72010af9e50
Edmundo0994 <esala.094@gmail.com>
2022-02-22 12:55:49 -0500

    [FIX] publicpath for bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    filename: '/shopExample/bundle.js',
+    publicPath: '/shopExample/'
   },
   resolve: {
     extensions: ['.js', '.jsx'],
