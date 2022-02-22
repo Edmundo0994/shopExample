fb460f7d9f58ecd945c8597ddf8b368ea356b345
Edmundo0994 <esala.094@gmail.com>
2022-02-22 12:58:29 -0500

    [FIX] Trying to resolve deploy issues

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '/shopExample/bundle.js',
+    filename: 'shopExample/bundle.js',
     publicPath: '/shopExample/'
   },
   resolve: {
