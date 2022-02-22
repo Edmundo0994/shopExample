eed816bc3d930be0e29279af3879d71eeef4423a
Edmundo0994 <esala.094@gmail.com>
2022-02-22 15:45:58 -0500

    Fixing some bugs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'shopExample/bundle.js',
+    filename: 'bundle.js',
     publicPath: '/shopExample/'
   },
   resolve: {
